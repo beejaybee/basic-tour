c382a2ffa26d32d4a51c7153d36a4f4590081903
Oyewo Bolaji Sodiq <oyewob@gmail.com>
2021-11-06 12:43:18 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tours-project'
 function App() {
+  const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false)
+
+  const fetchTours = async () => {
+    try {
+      setLoading(true);
+      setError(false);
+
+      const response = await (await fetch(url)).json();
+
+      setTours(response);
+
+      setLoading(false);
+
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  }
+
+  useEffect(()=> {
+    fetchTours();
+  }, []);
+
+  console.log(tours);
+
+  const removeTour = id => {
+    const newTours = tours.filter(tour => tour.id !== id);
+    setTours(newTours);
+  }
+
+  if(loading) return <main><Loading /></main>
+  if(error) return <main>Something went wrong ....</main>
+
+  if(tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+
+          <h2>
+            no tours left
+          </h2>
+          <button onClick={fetchTours} className="btn">refresh</button>
+
+        </div>
+      </main>
+    )
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <Tours tours={tours} removeTour={removeTour}/>
+    </main>
+  ) 
 }
 
-export default App;
+export default App
